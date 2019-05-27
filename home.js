be22080257447038d165167e3b4f764866dd57ca
adriancolman <adriancolman1985@gmail.com>
2019-05-27 00:46:59 -0300

    creandoPerfil

---
+++ home.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // Codigo a ejecutarse una vez que se carga la pagina
+
+    // $("#posts-container").html("<h1>Hola jQuery</h1>");
+    
+    $("#post-button").click(function() {
+        $("#posts-container").append(
+            +
+                        $("#post-message").val() +
+                    '</p>' +
+                '</div>' +
+            '</div>'
+        );
+    });
+
+});
