097a1ad956f0b2c3300ed87d6bcdca2d149b516a
adriancolman <adriancolman1985@gmail.com>
2019-05-31 21:54:25 -0300

    pocos-avances

--- home.js
+++ home.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
-    // Codigo a ejecutarse una vez que se carga la pagina
+  // Codigo a ejecutarse una vez que se carga la pagina
 
-    // $("#posts-container").html("<h1>Hola jQuery</h1>");
-    
-    $("#post-button").click(function() {
-        $("#posts-container").append(
-            +
-                        $("#post-message").val() +
-                    '</p>' +
-                '</div>' +
-            '</div>'
-        );
-    });
+  // $("#posts-container").html("<h1>Hola jQuery</h1>");
 
-    $("#foto-perfil").click(function(){
-        $("#button-foto-perfil").hide()
+  $("#post-button").click(function() {
+    $("#posts-container").append(
+      +$("#post-message").val() + "</p>" + "</div>" + "</div>"
+    );
+  });
 
-    })
+  $("#foto-perfil").click(function() {
+    $("#button-foto-perfil").hide();
+  });
 
+  /*login*/
+
+  $("#usuario-login-name").val();
+  $("#inlineFormInputGroupUsername2").val();
+
+  var usuario = $("#usuario-login-name").val();
+
+
+  $(#enter-login).click(function(){
+  if (usuario === "adrian") alert("usuario correcto");})
+  
 });
---
+++ perfil.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#boton-loco").click(function() {
+    alert("lo has borrado");
+  });
+});
